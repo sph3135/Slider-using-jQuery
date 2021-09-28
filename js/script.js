8d6ce42a661f84aa172241cca8687e312a8dde24
Sudeep P H <sudeepph360@gmail.com>
2021-09-28 17:21:14 +0530

    Simple slider using jQuery and JavaScript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: true,
+        arrows: true,
+
+    });
+  });
+
